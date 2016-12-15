c7a61ee98ffa910ea58edea40e9d5e1b7ceeb049
raditotev <raditotev@gmail.com>
2016-12-15 11:48:32 +0000

    Set up links and routing for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -21,7 +21,7 @@ class ReviewsController < ApplicationController
 
     respond_to do |format|
       if @review.save
-        format.html { redirect_to root_url, notice: 'Review was successfully created.' }
+        format.html { redirect_to @garage, notice: 'Review was successfully created.' }
         format.json { render :show, status: :created, location: @review }
       else
         format.html { render :new }
